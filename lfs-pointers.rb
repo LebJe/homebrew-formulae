fff3ce62af9bfd46dba0c497e7dbb85be5043e5b
Jeff L <51171427+LebJe@users.noreply.github.com>
2020-04-16 21:28:54 -0400

    Initial Commit

---
+++ lfs-pointers.rb
@@ -0,0 +1,19 @@
+class LfsPointers < Formula
+	desc "A tool that allows you to convert a directory of large files to Git LFS pointers."
+	homepage "https://github.com/LebJe/LFSPointers"
+	url "https://github.com/LebJe/LFSPointers.git",
+		:tag => "0.0.8"
+	head "https://github.com/LebJe/LFSPointers.git"
+  
+	depends_on :xcode => ["11.0", :build]
+  
+	def install
+		system "swift build --configuration release --disable-sandbox"
+		system "mv", ".build/release/LFSPointers", "LFSPointers"
+		bin.install "LFSPointers"
+	end
+  
+	test do
+	  system "LFSPointers --help"
+	end
+  end
