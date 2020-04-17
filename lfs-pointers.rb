bf3078bc84a0ff06654fb019db059f2b191664ff
Jeff L <LebJe@users.noreply.github.com>
2020-04-17 06:58:33 -0400

    Update lfs-pointers.rb

--- lfs-pointers.rb
+++ lfs-pointers.rb
@@ -2,10 +2,10 @@ class LfsPointers < Formula
 	desc "A tool that allows you to convert a directory of large files to Git LFS pointers."
 	homepage "https://github.com/LebJe/LFSPointers"
 	url "https://github.com/LebJe/LFSPointers.git",
-		:tag => "0.0.8"
+		:tag => "0.0.9"
 	head "https://github.com/LebJe/LFSPointers.git"
   
-	depends_on :xcode => ["11.0", :build]
+	depends_on :xcode => ["11.4", :build]
   
 	def install
 		system "swift build --configuration release --disable-sandbox"
