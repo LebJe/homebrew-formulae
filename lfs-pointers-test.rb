be59b0c7bf797f9fd0b32aa4b301355354a0bf2b
LebJe <51171427+LebJe@users.noreply.github.com>
2020-08-10 17:15:55 -0400

    Update

--- lfs-pointers-test.rb
+++ lfs-pointers-test.rb
@@ -16,11 +16,11 @@ class LfsPointersTest < Formula
 		# Completion Scripts.
 
 		# ZSH.
-		File.write("_LFSPointers", shell_output("LFSPointers --generate-completion-script zsh"))
+		File.write("_LFSPointers", `LFSPointers --generate-completion-script zsh`))
 		zsh_completion.install "_LFSPointers"
 
 		# Bash.
-		File.write("LFSPointers.bash", shell_output("LFSPointers --generate-completion-script bash"))
+		File.write("LFSPointers.bash", `LFSPointers --generate-completion-script bash`))
 		bash_completion.install "LFSPointers.bash"
 	end
   
