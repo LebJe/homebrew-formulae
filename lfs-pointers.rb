4a860d829768a6fee3998e6303a0d98278887f66
LebJe <51171427+LebJe@users.noreply.github.com>
2020-08-23 11:30:48 -0400

    Use prebuilt binary.

--- lfs-pointers.rb
+++ lfs-pointers.rb
@@ -1,29 +1,22 @@
-class LfsPointers < Formula
+class LfsPointersTest < Formula
 	desc "A tool that allows you to convert a directory of large files to Git LFS pointers."
 	homepage "https://lebje.github.io/LFSPointers"
-	url "https://github.com/LebJe/LFSPointers.git",
-		:tag => "0.12.7"
+	url "https://github.com/LebJe/LFSPointers/releases/download/0.12.8/lfspointers-macos-x86_64.tar.gz"
 	head "https://github.com/LebJe/LFSPointers.git"
-
+    sha256 "5fdfe6a226a37490970b3c09f6069394e42789f6158451ef218c4d163592eda5"
 	license "MIT"
   
 	def install
-		system "swift", "build", "--configuration", "release", "--disable-sandbox"
-		system "mv", ".build/release/LFSPointers", "LFSPointers"
 		bin.install "LFSPointers"
 
 		# Completion Scripts.
-
-		zshC = %x[#{bin}/LFSPointers --generate-completion-script zsh]
-
 		# ZSH.
+		zshC = %x[#{bin}/LFSPointers --generate-completion-script zsh]
 		File.write("_LFSPointers", zshC)
 		zsh_completion.install "_LFSPointers"
 
 		# Bash.
-
 		bashC = %x[#{bin}/LFSPointers --generate-completion-script bash]
-
 		File.write("LFSPointers.bash", bashC)
 		bash_completion.install "LFSPointers.bash"
 	end
