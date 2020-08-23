c275a062db07774c25260b0a22c89324a871b4ac
LebJe <51171427+LebJe@users.noreply.github.com>
2020-08-23 11:24:33 -0400

    Use prebuilt binary.

---
+++ lfs-pointers-test.rb
@@ -0,0 +1,27 @@
+class LfsPointers < Formula
+	desc "A tool that allows you to convert a directory of large files to Git LFS pointers."
+	homepage "https://lebje.github.io/LFSPointers"
+	url "https://github.com/LebJe/LFSPointers/releases/download/0.12.8/lfspointers-macos-x86_64.tar.gz"
+	head "https://github.com/LebJe/LFSPointers.git"
+
+	license "MIT"
+  
+	def install
+		bin.install "LFSPointers"
+
+		# Completion Scripts.
+		# ZSH.
+		zshC = %x[#{bin}/LFSPointers --generate-completion-script zsh]
+		File.write("_LFSPointers", zshC)
+		zsh_completion.install "_LFSPointers"
+
+		# Bash.
+		bashC = %x[#{bin}/LFSPointers --generate-completion-script bash]
+		File.write("LFSPointers.bash", bashC)
+		bash_completion.install "LFSPointers.bash"
+	end
+  
+	test do
+	  system "LFSPointers --help"
+	end
+  end
