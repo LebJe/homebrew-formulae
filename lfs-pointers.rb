9e648c6e3d2d0f9f74878b3ee5ae6a191ca38469
LebJe <51171427+LebJe@users.noreply.github.com>
2020-09-16 11:58:25 -0400

    Testing

--- lfs-pointers.rb
+++ lfs-pointers.rb
@@ -1,32 +1,32 @@
 class LfsPointers < Formula
-	desc "A tool that allows you to convert a directory of large files to Git LFS pointers."
-	homepage "https://lebje.github.io/LFSPointers"
-    url "https://github.com/LebJe/LFSPointers/releases/download/1.0.0/lfspointers-macos-x86_64.tar.gz"
-	head "https://github.com/LebJe/LFSPointers.git"
-    sha256 "0793ce7ef26c0a9c577edf40d9da3d86a418aa5b6308e4692112a39f2d0a7304"
-	license "MIT"
-  
-	def install
-		bin.install "LFSPointers"
+  desc "Tool that allows you to convert a directory of large files to Git LFS pointers"
+  homepage "https://lebje.github.io/LFSPointers"
+  url "https://github.com/LebJe/LFSPointers/releases/download/1.0.0/lfspointers-macos-x86_64.tar.gz"
+  sha256 "0793ce7ef26c0a9c577edf40d9da3d86a418aa5b6308e4692112a39f2d0a7304"
+  license "MIT"
+  head "https://github.com/LebJe/LFSPointers.git"
 
-		# Completion Scripts.
-		# ZSH.
-		zshC = %x[#{bin}/LFSPointers --generate-completion-script zsh]
-		File.write("_LFSPointers", zshC)
-		zsh_completion.install "_LFSPointers"
+  def install
+    bin.install "LFSPointers"
 
-		# Bash.
-		bashC = %x[#{bin}/LFSPointers --generate-completion-script bash]
-		File.write("LFSPointers.bash", bashC)
-		bash_completion.install "LFSPointers.bash"
+    # Completion Scripts.
+    # ZSH.
+    zshc = `#{bin}/LFSPointers --generate-completion-script zsh`
+    File.write("_LFSPointers", zshc)
+    zsh_completion.install "_LFSPointers"
 
-		# Fish.
-		fishC = %x[#{bin}/LFSPointers --generate-completion-script fish]
-        File.write("LFSPointers.fish", fishC)
-		fish_completion.install "LFSPointers.fish"
-	end
-  
-	test do
-	  system "LFSPointers --help"
-	end
+    # Bash.
+    bashc = `#{bin}/LFSPointers --generate-completion-script bash`
+    File.write("LFSPointers.bash", bashc)
+    bash_completion.install "LFSPointers.bash"
+
+    # Fish.
+    fishc = `#{bin}/LFSPointers --generate-completion-script fish`
+    File.write("LFSPointers.fish", fishc)
+    fish_completion.install "LFSPointers.fish"
   end
+
+  test do
+    system "LFSPointers --help"
+  end
+end
