0cb6c000117babb6bf964f0f86ac3c3da1e7bc64
LebJe <51171427+LebJe@users.noreply.github.com>
2020-10-21 19:40:38 +0000

    Testing.

--- lfs-pointers-test.rb
+++ lfs-pointers-test.rb
@@ -2,16 +2,9 @@ class LfsPointersTest < Formula
   desc "Tool that allows you to convert a directory of large files to Git LFS pointers"
   homepage "https://lebje.github.io/LFSPointers"
   url "https://github.com/LebJe/LFSPointers.git",
-	  tag: "1.0.7"
-  #sha256 "0793ce7ef26c0a9c577edf40d9da3d86a418aa5b6308e4692112a39f2d0a7304"
+	  tag: "1.0.8"
   license "MIT"
   head "https://github.com/LebJe/LFSPointers.git"
-
-  bottle do
-    root_url "https://github.com/LebJe/LFSPointersTestHomebrewBottle/releases/download/1.0.7"
-    cellar :any_skip_relocation
-    sha256 "433804c641d3ada9d30e3701ae78f804c3095e404ef95138762ab1a8ac2fa7f2" => :big_sur
-  end
 
   def install
     system "./build.sh"
