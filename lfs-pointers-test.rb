5af71525a14c4f22b818eea5ce5f3b4d1e4e0fbc
Jeff L <51171427+LebJe@users.noreply.github.com>
2020-11-26 15:32:27 -0500

    Update lfs-pointers-test.rb

--- lfs-pointers-test.rb
+++ lfs-pointers-test.rb
@@ -1,37 +1,35 @@
-class LfsPointersTest < Formula
+class LfsPointers < Formula
   desc "Tool that allows you to convert a directory of large files to Git LFS pointers"
-  homepage "https://lebje.github.io/LFSPointers"
+  homepage "https://github.com/LebJe/LFSPointers"
   url "https://github.com/LebJe/LFSPointers.git",
     :tag => "1.0.8"
   license "MIT"
-  head "https://github.com/LebJe/LFSPointers.git"
+  head "https://github.com/LebJe/LFSPointers.git", :branch => "main"
 
   bottle do
-    root_url "https://github.com/LebJe/LFSPointersTestHomebrewBottle/releases/download/1.0.8"
+    root_url "https://github.com/LebJe/LFSPointers/releases/download/1.0.8"
     cellar :any_skip_relocation
-    sha256 "da77699fe835a8e91c0c65702cced18802d731d8a8dee3d07c44d0ef21464afa" => :big_sur
+    sha256 "2f55b1b0cd83e2b9c6f054eb1d3a938f3dd95e01ef12a718e873e9e088109d62" => :big_sur
   end
 
   def install
     system "./build.sh"
 
-    system "mv .build/release/LFSPointers ."
-
-    bin.install "LFSPointers"
+    system "ln -s .build/release/LFSPointers /usr/local/bin"
 
     # Completion Scripts.
     # ZSH.
-    zshc = `#{bin}/LFSPointers --generate-completion-script zsh`
+    zshc = `LFSPointers --generate-completion-script zsh`
     File.write("_LFSPointers", zshc)
     zsh_completion.install "_LFSPointers"
 
     # Bash.
-    bashc = `#{bin}/LFSPointers --generate-completion-script bash`
+    bashc = `LFSPointers --generate-completion-script bash`
     File.write("LFSPointers.bash", bashc)
     bash_completion.install "LFSPointers.bash"
 
     # Fish.
-    fishc = `#{bin}/LFSPointers --generate-completion-script fish`
+    fishc = `LFSPointers --generate-completion-script fish`
     File.write("LFSPointers.fish", fishc)
     fish_completion.install "LFSPointers.fish"
   end
